958f0ac4e323af281739f42c825ba2da82d14601
박준영 <qkrwnsdud@kookmin.ac.kr>
2023-10-25 12:21:01 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 420 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -0,0 +1,20 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for i in range(T):
+    x, y = map(int, input().split())
+    d = y - x
+    n = 0
+
+    while True:
+        if d <= n * (n + 1):
+            break
+        n += 1
+    if d <= n**2:
+        print(n * 2 - 1)
+    else:
+        print(n * 2)

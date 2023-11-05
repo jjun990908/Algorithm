f44b97ac23903a37fbe07b368165f5fbcd0ae231
박준영 <qkrwnsdud@kookmin.ac.kr>
2023-11-06 00:23:22 +0900

    [Silver I] Title: 점프, Time: 64 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,19 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * N for _ in range(N)]
+dp[0][0] = 1
+
+for i in range(N):
+    for j in range(N):
+        if i == N - 1 and j == N - 1:
+            print(dp[i][j])
+            break
+        if j + graph[i][j] < N:
+            dp[i][j + graph[i][j]] += dp[i][j]
+        if i + graph[i][j] < N:
+            dp[i + graph[i][j]][j] += dp[i][j]

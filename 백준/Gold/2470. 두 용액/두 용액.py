b198222a298fafcfa5d3570e13a5dd7e229dbd7a
박준영 <qkrwnsdud@kookmin.ac.kr>
2023-11-07 12:41:12 +0900

    [Gold V] Title: 두 용액, Time: 148 ms, Memory: 43844 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+arr = sorted(list(map(int, input().split())))
+
+start = 0
+end = N - 1
+answerSum = abs(arr[0] + arr[end])
+answer = [arr[0], arr[end]]
+
+while start < end:
+    left = arr[start]
+    right = arr[end]
+    sum = left + right
+
+    if abs(sum) < answerSum:
+        answerSum = abs(sum)
+        answer = [left, right]
+        if answerSum == 0:
+            break
+    if sum < 0:
+        start += 1
+    else:
+        end -= 1
+print(*answer)
